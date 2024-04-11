e13bae298eead723996660fe3888cf7c4dc9d9ab
Dennis <dennis.dombrovskij@gmx.de>
2024-04-11 17:37:15 +0200

    base dashboard with logo and sidebars

---
+++ Dashboard/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.sidebar.text('Home') # Image from Elena
+
+st.sidebar.text('Capstone Project')
+st.sidebar.text('Dataset & Features')
+st.sidebar.text('Models')
+st.sidebar.text('About EyeTism')
+st.sidebar.text('About us')
+st.sidebar.text('---')
+st.sidebar.text('Patients')
+st.sidebar.text('Recording')
+st.sidebar.text('Evaluation')
+
+
+
+
+st.image('images/Logo_Eyetism.png')
