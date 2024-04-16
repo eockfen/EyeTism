684f064f4564faee113c2b1c36e91f8610418c6e
Adam Zabicki <azabicki@posteo.de>
2024-04-16 22:26:53 +0200

    restructure patients

--- Dashboard/pages/patients.py
+++ Dashboard/pages/patients.py
@@ -10,8 +10,8 @@ ut.create_menu()
 
 # page style ---------------------------------------------
 st.title("Manage Patients")
-tab1, tab2, tab3 = st.tabs(
-    [s.center(18, "\u2001") for s in ["List Patients", "Edit Patients", "New Patient"]]
+tab1, tab2 = st.tabs(
+    [s.center(30, "\u2001") for s in ["List Patients", "Manage Patients"]]
 )
 
 # list all patients ---------------------------------------------
@@ -39,20 +39,18 @@ with tab2:
 
     # show db
     st.session_state.pat_db_update = st.data_editor(
-        st.session_state.pat_db,
+        st.session_state.pat_db[["id", "name", "age"]],
         use_container_width=True,
         hide_index=True,
         disabled=["id", "n_rec", "last_rec"],
         column_config={
-            "id": st.column_config.Column(label="id", width="small"),
+            "id": st.column_config.Column(label="id", width=1),
             "name": st.column_config.TextColumn(
-                label="Name", width="medium", max_chars=50, required=True
+                label="Name", width="large", max_chars=50, required=True
             ),
             "age": st.column_config.NumberColumn(
-                label="Age", min_value=1, max_value=99, required=True
+                label="Age", width="medium", min_value=1, max_value=99, required=True
             ),
-            "n_rec": st.column_config.Column(label="# of rec."),
-            "last_rec": st.column_config.Column(label="last rec."),
         },
     )
 
@@ -66,57 +64,60 @@ with tab2:
         time.sleep(1)
         container_update.empty()
 
-    # delete patient entry -----------
-    st.subheader("Delete patient entry")
+    ut.h_spacer(5)
 
-    sel_patient = st.selectbox(
-        "Select patient to delete:",
-        [
-            f"{int(r['id'])}: {r['name']} (age: {int(r['age'])})"
-            for (_, r) in st.session_state.pat_db.iterrows()
-        ],
-        label_visibility="collapsed",
-    )
+    col_new, col_del = st.columns([1, 1], gap="large")
+    with col_new:
+        # add new patient entry ---------------------------------
+        st.subheader("Create new patient entry")
 
-    # button
-    deleted = st.button("Delete Patient", on_click=etf.del_patient, args=(sel_patient,))
+        with st.form("add_patient", clear_on_submit=True):
+            # name
+            name = st.text_input("Name:", value="", max_chars=50)
 
-    # feedback
-    container_del = st.empty()
-    if deleted:
-        container_del.success("Patient deleted successfully.")
-        time.sleep(1)
-        container_del.empty()
+            # age
+            _, col_age, _ = st.columns([0.03, 0.5, 0.46], gap="small")
+            with col_age:
+                age = st.slider("Age:", min_value=1, max_value=99)
 
+            # submit button
+            submitted = st.form_submit_button("Save Patient")
 
-# add new patients ---------------------------------------------
-with tab3:
-    st.subheader("Create new patient entry")
+            # handle submission
+            if submitted:
+                status = etf.add_pat(name, age)
+                container_add = st.empty()
+                if status:
+                    container_add.success("New patient saved successfully.")
+                    time.sleep(1)
+                    container_add.empty()
+                    st.rerun()
+                else:
+                    container_add.error("... patient was not saved...")
+                    time.sleep(2)
 
-    with st.form("add_patient", clear_on_submit=True):
-        # name
-        name = st.text_input("Name:", value="", max_chars=50)
+    with col_del:
+        # delete patient entry ---------------------------------
+        st.subheader("Delete patient entry")
 
-        # age
-        _, col_age, _ = st.columns([0.03, 0.5, 0.46], gap="small")
-        with col_age:
-            age = st.slider("Age:", min_value=1, max_value=99)
+        with st.form("del_patient", clear_on_submit=True):
+            sel_patient = st.selectbox(
+                "Select patient to delete:",
+                st.session_state.patient_list,
+                label_visibility="collapsed",
+            )
 
-        # submit button
-        submitted = st.form_submit_button("Save Patient")
+            # submit button
+            deleted = st.form_submit_button(
+                "Delete Patient", on_click=etf.del_patient, args=(sel_patient,)
+            )
 
-        # handle submission
-        if submitted:
-            status = etf.add_pat(name, age)
-            container_add = st.empty()
-            if status:
-                container_add.success("New patient saved successfully.")
-                time.sleep(1)
-                container_add.empty()
-                st.rerun()
-            else:
-                container_add.error("... patient was not saved...")
-                time.sleep(2)
+        # feedback
+        container_del = st.empty()
+        if deleted:
+            container_del.success("Patient deleted successfully.")
+            time.sleep(1)
+            container_del.empty()
 
 
 # ------------------------------------------------------------
